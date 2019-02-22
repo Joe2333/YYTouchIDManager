22603b2ea2fe2daa13311c59e2d0e75674ecc79c
pengyuanyang <pengyuanyang@lattebank.com>
2019-02-22 18:37:56 +0800

    新增类

---
+++ YYTouchIDManagerDemo/Text2.h
@@ -0,0 +1,17 @@
+//
+//  Text2.h
+//  YYTouchIDManagerDemo
+//
+//  Created by Joe on 2019/2/22.
+//  Copyright © 2019 Joe. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Text2 : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ YYTouchIDManagerDemo/Text2.m
@@ -0,0 +1,13 @@
+//
+//  Text2.m
+//  YYTouchIDManagerDemo
+//
+//  Created by Joe on 2019/2/22.
+//  Copyright © 2019 Joe. All rights reserved.
+//
+
+#import "Text2.h"
+
+@implementation Text2
+
+@end
