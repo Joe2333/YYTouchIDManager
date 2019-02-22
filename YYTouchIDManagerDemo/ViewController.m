c62ba1f1572690eddd5b99f305134c2bf01bfffc
pengyuanyang <pengyuanyang@lattebank.com>
2019-02-22 17:01:02 +0800

    Revert "test1"
    This reverts commit e03ace4985d0aaaaebf79cd2b5d020961ceca3c2.

--- YYTouchIDManagerDemo/ViewController.m
+++ YYTouchIDManagerDemo/ViewController.m
@@ -17,9 +17,6 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
-    NSLog(@"做验证做验证");
-    
     // Do any additional setup after loading the view, typically from a nib.
 }
 - (IBAction)action:(id)sender {
