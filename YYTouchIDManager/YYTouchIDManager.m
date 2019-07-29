b0698db2c374e980862a9ae7327104d6e1587871
彭远洋 <pengyuanyang@sensorsdata.cn>
2019-07-29 10:47:48 +0800

    删除依赖库

--- YYTouchIDManager/YYTouchIDManager.m
+++ YYTouchIDManager/YYTouchIDManager.m
@@ -7,87 +7,10 @@
 //
 
 #import "YYTouchIDManager.h"
-#import <LocalAuthentication/LocalAuthentication.h>
 
 @implementation YYTouchIDManager
 
 +(void)verifyTouchID:(void(^)())verifySuccess login:(void(^)())verifyLogin
 {
-    //初始化上下文对象
-    LAContext* context = [[LAContext alloc] init];
-    //错误对象
-    NSError* error = nil;
-    NSString* result = @"这是用来测试的Touch ID";
-    //首先使用canEvaluatePolicy 判断设备支持状态
-    if ([context canEvaluatePolicy:LAPolicyDeviceOwnerAuthenticationWithBiometrics error:&error]) {
-        //支持指纹验证
-        [context evaluatePolicy:LAPolicyDeviceOwnerAuthenticationWithBiometrics localizedReason:result reply:^(BOOL success, NSError *error) {
-            if (success) {
-                //验证成功，主线程处理UI
-                verifySuccess();
-            }
-            else
-            {
-                NSLog(@"%@",error.localizedDescription);
-                switch (error.code) {
-                    case LAErrorSystemCancel:
-                    {
-                        NSLog(@"Authentication was cancelled by the system");
-                        //切换到其他APP，系统取消验证Touch ID
-                        NSLog(@"切换到其他APP");
-                        break;
-                    }
-                    case LAErrorUserCancel:
-                    {
-                        NSLog(@"Authentication was cancelled by the user");
-                        NSLog(@"用户取消验证Touch ID");
-                        //用户取消验证Touch ID
-                        break;
-                    }
-                    case LAErrorUserFallback:
-                    {
-                        NSLog(@"User selected to enter custom password");
-                        [[NSOperationQueue mainQueue] addOperationWithBlock:^{
-                            //用户选择输入密码，切换主线程处理
-                            NSLog(@"用户选择输入密码");
-                            verifyLogin();
-                        }];
-                        break;
-                    }
-                    default:
-                    {
-                        [[NSOperationQueue mainQueue] addOperationWithBlock:^{
-                            //其他情况，切换主线程处理
-                            NSLog(@"其他情况，切换主线程处理");
-                        }];
-                        break;
-                    }
-                }
-            }
-        }];
-    }
-    else
-    {
-        //不支持指纹识别，LOG出错误详情
-        switch (error.code) {
-            case LAErrorTouchIDNotEnrolled:
-            {
-                NSLog(@"TouchID is not enrolled");
-                break;
-            }
-            case LAErrorPasscodeNotSet:
-            {
-                NSLog(@"A passcode has not been set");
-                break;
-            }
-            default:
-            {
-                NSLog(@"TouchID not available");
-                break;
-            }
-        }
-        NSLog(@"%@",error.localizedDescription);
-    }
-
 }
 @end
