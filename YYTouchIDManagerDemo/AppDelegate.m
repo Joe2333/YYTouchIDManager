9739241c15e2437bfc01c8a8d913005e75c6b55b
彭远洋 <pengyuanyang@sensorsdata.cn>
2019-12-31 17:11:13 +0800

    merge commit

--- YYTouchIDManagerDemo/AppDelegate.m
+++ YYTouchIDManagerDemo/AppDelegate.m
@@ -17,6 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    //commit 2
     return YES;
 }
 
